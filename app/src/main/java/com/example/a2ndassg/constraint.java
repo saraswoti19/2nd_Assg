aa2d2076a6d8495ae80f95a2e83a24bde5e594a0
saraswoti19 <sarastha16@gmail.com>
2019-10-20 15:45:05 +0545

    Initial commit

---
+++ app/src/main/java/com/example/a2ndassg/constraint.java
@@ -0,0 +1,23 @@
+package com.example.a2ndassg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+
+
+public class constraint extends AppCompatActivity {
+
+
+    EditText crunchifyEditText;
+    ImageView crunchifyImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+    }
+}
---
+++ app/src/main/java/com/example/a2ndassg/relativelayout.java
@@ -0,0 +1,14 @@
+package com.example.a2ndassg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class relativelayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relativelayout);
+    }
+}
